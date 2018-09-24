bd0537313032a9813f8cb6fea72dcb55eb08bc73
wonderxiao <942286384@qq.com>
2018-09-24 17:51:13 +0800

    first commit init

---
+++ src/main/java/cn/wonderxiao/fivechess/FivechessApplication.java
@@ -0,0 +1,12 @@
+package cn.wonderxiao.fivechess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivechessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FivechessApplication.class, args);
+    }
+}
