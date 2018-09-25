95e63f4aaf2e0ef240024ddf1632de881439fdfb
wonderxiao <942286384@qq.com>
2018-09-25 11:18:37 +0800

    update

--- src/main/java/cn/wonderxiao/fivechess/controller/HelloWorld.java
+++ src/main/java/cn/wonderxiao/fivechess/controller/HelloWorld.java
@@ -1,12 +1,16 @@
 package cn.wonderxiao.fivechess.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
+    Logger logger = LoggerFactory.getLogger(HelloWorld.class);
     @RequestMapping("/")
     public String index() {
+        logger.info("hello world");
         return "Hello Docker!";
     }
 }
